15e9b729375d87cceeba7cdbe649f2c1c5ffde7c
leijialiang <ljl519525771@qq.com>
2022-11-08 15:15:54 +0800

    feat: 忽略 dist

--- packages/utils/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 程序暂停运行
- * @param ms 暂停运行的时间（毫秒）
- */
-declare const sleep: (ms: number) => Promise<void>;
-declare const a: string;
-
-export { a, sleep };
--- packages/utils/typings/index.d.ts
+++ packages/utils/typings/index.d.ts
@@ -3,4 +3,3 @@
  * @param ms 暂停运行的时间（毫秒）
  */
 export declare const sleep: (ms: number) => Promise<void>;
-export declare const a: string;
